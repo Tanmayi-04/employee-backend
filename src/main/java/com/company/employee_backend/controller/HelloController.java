f4310aa446cc1bbdbd35dc953f8db882bb41b0a9
Tanmayi Bandaru <tanmayirvg@gmail.com>
2026-01-09 18:17:29 +0000

    Updated HelloController with small change

--- src/main/java/com/company/employee_backend/controller/HelloController.java
+++ src/main/java/com/company/employee_backend/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.capgemini.employee_backend.controller;
+package com.company.employee_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
